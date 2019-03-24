fa18bce49808bbd34236d32398e77612659c19e6
Sergey V. Karpesh <walhi@walhi.ru>
2019-03-24 03:04:54 +0300

    Focus and Exposure.

---
+++ webcamControl/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace webcamControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.updateButton = new System.Windows.Forms.Button();
+            this.overWindowToggleButton = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(162, 12);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(146, 23);
+            this.updateButton.TabIndex = 1;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // overWindowToggleButton
+            // 
+            this.overWindowToggleButton.Location = new System.Drawing.Point(12, 12);
+            this.overWindowToggleButton.Name = "overWindowToggleButton";
+            this.overWindowToggleButton.Size = new System.Drawing.Size(144, 23);
+            this.overWindowToggleButton.TabIndex = 2;
+            this.overWindowToggleButton.Text = "Over all windows";
+            this.overWindowToggleButton.UseVisualStyleBackColor = true;
+            this.overWindowToggleButton.CheckedChanged += new System.EventHandler(this.overWindowToggleButton_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 49);
+            this.Controls.Add(this.overWindowToggleButton);
+            this.Controls.Add(this.updateButton);
+            this.Name = "Form1";
+            this.Text = "Webcam Focus control";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.CheckBox overWindowToggleButton;
+    }
+}
+
---
+++ webcamControl/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DirectShowLib;
+
+
+namespace webcamControl
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DsDevice[] capDev;
+            IFilterGraph2 m_FilterGraph = new FilterGraph() as IFilterGraph2;
+            IBaseFilter pDeviceFilter = null;
+            IAMCameraControl pCameraControl;
+            CameraControlFlags pCapsFlags;
+
+            int pSteppingDelta;
+            int pMin;
+            int pMax;
+            int pDefault;
+
+            int yLocation = 40;
+            const int yStep = 113;
+
+            overWindowToggleButton.Appearance = Appearance.Button;
+
+            // TODO
+            updateButton.Visible = false;
+
+            // Get the collection of video devices
+            capDev = DsDevice.GetDevicesOfCat(FilterCategory.VideoInputDevice);
+
+            foreach (DsDevice d in capDev)
+            {
+                //listBox1.Items.Add(d.Name);
+                int hr = m_FilterGraph.AddSourceFilterForMoniker(d.Mon, null, d.Name, out pDeviceFilter);
+                pCameraControl = pDeviceFilter as IAMCameraControl;
+                if (pCameraControl != null)
+                {
+                    GroupBox gb = new GroupBox();
+                    CheckBox cbFocus = new CheckBox();
+                    CheckBox cbExposure = new CheckBox();
+                    Button butForce = new Button();
+                    TrackBar tBarFocus = new TrackBar();
+                    TrackBar tBarExposure = new TrackBar();
+
+                    gb.Text = d.Name;
+                    gb.Width = 295;
+                    gb.Height = 106;
+                    gb.Location = new Point(13, yLocation);
+
+
+                    pCameraControl.GetRange(CameraControlProperty.Focus, out pMin, out pMax, out pSteppingDelta, out pDefault, out pCapsFlags);
+
+                    cbFocus.Text = "Auto focus";
+                    cbFocus.Width = 78;
+                    cbFocus.Height = 17;
+                    cbFocus.Location = new Point(6, 23);
+                    cbFocus.Tag = butForce;
+                    cbFocus.Checked = true;
+                    cbFocus.CheckedChanged += new EventHandler(this.checkBoxFocus_CheckedChanged);
+
+                    butForce.Text = "Force";
+                    butForce.Width = 75;
+                    butForce.Height = 23;
+                    butForce.Location = new Point(90, 19);
+                    //butForce.Enabled = false;
+                    butForce.Tag = pCameraControl;
+                    butForce.Click += new EventHandler(this.ButtonForce_Click);
+
+                    // TODO
+                    //butBind.Visible = false;
+                    //butBind.Text = "Bind key";
+                    //butBind.Width = 75;
+                    //butBind.Height = 23;
+                    //butBind.Location = new Point(214, 19);
+
+                    tBarFocus.Minimum = pMin;
+                    tBarFocus.Maximum = pMax;
+                    tBarFocus.Value = pDefault;
+                    tBarFocus.Name = "TrackBarFocus";
+                    tBarFocus.AutoSize = false;
+                    tBarFocus.Width = 283;
+                    tBarFocus.Height = 23;
+                    tBarFocus.TickFrequency = pSteppingDelta;
+                    tBarFocus.Location = new Point(6, 46);
+                    tBarFocus.Tag = pCameraControl;
+                    tBarFocus.Enabled = false;
+                    tBarFocus.Scroll += new EventHandler(this.TrackBarFocus_Scroll);
+
+
+                    pCameraControl.GetRange(CameraControlProperty.Exposure, out pMin, out pMax, out pSteppingDelta, out pDefault, out pCapsFlags);
+
+                    cbExposure.Text = "Auto exposure";
+                    cbExposure.Width = 98;
+                    cbExposure.Height = 17;
+                    cbExposure.Location = new Point(191, 23);
+                    cbExposure.Tag = butForce;
+                    cbExposure.Checked = true;
+                    cbExposure.CheckedChanged += new EventHandler(this.checkBoxExposure_CheckedChanged);
+
+                    tBarExposure.Minimum = pMin;
+                    tBarExposure.Maximum = pMax;
+                    tBarExposure.Value = pDefault;
+                    tBarExposure.Name = "TrackBarExposure";
+                    tBarExposure.AutoSize = false;
+                    tBarExposure.Width = 283;
+                    tBarExposure.Height = 23;
+                    tBarExposure.TickFrequency = pSteppingDelta;
+                    tBarExposure.Location = new Point(6, 73);
+                    tBarExposure.Tag = pCameraControl;
+                    tBarExposure.Enabled = false;
+                    tBarExposure.Scroll += new EventHandler(this.TrackBarExposure_Scroll);
+
+                    gb.Controls.Add(cbFocus);
+                    gb.Controls.Add(butForce);
+                    gb.Controls.Add(cbExposure);
+                    gb.Controls.Add(tBarFocus);
+                    gb.Controls.Add(tBarExposure);
+                    Controls.Add(gb);
+
+                    yLocation += yStep;
+                }
+            }
+            this.Height = 13 + yLocation + 39;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private void TrackBarExposure_Scroll(object sender, EventArgs e)
+        {
+            TrackBar trackBar = (TrackBar)sender;
+            int Value = trackBar.Value - trackBar.Value % trackBar.TickFrequency;
+            trackBar.Value = Value;
+            IAMCameraControl pCameraControl = (IAMCameraControl)trackBar.Tag;
+            pCameraControl.Set(CameraControlProperty.Exposure, Value, CameraControlFlags.Manual);
+        }
+
+        private void TrackBarFocus_Scroll(object sender, EventArgs e)
+        {
+            TrackBar trackBar = (TrackBar)sender;
+            int Value = trackBar.Value - trackBar.Value % trackBar.TickFrequency;
+            trackBar.Value = Value;
+            int inverceValue = trackBar.Maximum - Value + trackBar.Minimum;
+            IAMCameraControl pCameraControl = (IAMCameraControl)trackBar.Tag;
+            pCameraControl.Set(CameraControlProperty.Focus, inverceValue, CameraControlFlags.Manual);
+        }
+
+        private void ButtonForce_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            IAMCameraControl pCameraControl = (IAMCameraControl)btn.Tag;
+            pCameraControl.Set(CameraControlProperty.Focus, 0, CameraControlFlags.Manual);
+            pCameraControl.Set(CameraControlProperty.Focus, 0, CameraControlFlags.Auto);
+        }
+
+        private void checkBoxFocus_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox cb = (CheckBox)sender;
+            Button btn = (Button)cb.Tag;
+            TrackBar tbar = (TrackBar)cb.Parent.Controls.Find("TrackBarFocus", false)[0];
+            IAMCameraControl pCameraControl = (IAMCameraControl)btn.Tag;
+            btn.Enabled = cb.Checked;
+            tbar.Enabled = !cb.Checked;
+            if (cb.Checked)
+            {
+                pCameraControl.Set(CameraControlProperty.Focus, 0, CameraControlFlags.Auto);
+            }
+            else
+            {
+                pCameraControl.Set(CameraControlProperty.Focus, (tbar.Maximum - tbar.Value + tbar.Minimum), CameraControlFlags.Manual);
+            }
+        }
+
+        private void checkBoxExposure_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox cb = (CheckBox)sender;
+            Button btn = (Button)cb.Tag;
+            TrackBar tbar = (TrackBar)cb.Parent.Controls.Find("TrackBarExposure", false)[0];
+            IAMCameraControl pCameraControl = (IAMCameraControl)btn.Tag;
+            tbar.Enabled = !cb.Checked;
+            if (cb.Checked)
+            {
+                pCameraControl.Set(CameraControlProperty.Exposure, 0, CameraControlFlags.Auto);
+            }
+            else
+            {
+                pCameraControl.Set(CameraControlProperty.Exposure, tbar.Value, CameraControlFlags.Manual);
+            }
+        }
+        
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void overWindowToggleButton_CheckedChanged(object sender, EventArgs e)
+        {
+            TopMost = ((CheckBox)sender).Checked;
+        }
+    }
+}
